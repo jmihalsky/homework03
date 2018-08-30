395ddd9902b97a48b8282124be176275819a37b3
jmihalsky <jmihalsky@gmail.com>
2018-08-30 00:35:04 -0700

    8-30-2018 work commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var ltr_game = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var ltr_cmp = "";
+
+function cmp_sel(){
+    var cmp = Math.floor((Math.random()*25)+1);
+    ltr_cmp = ltr_game[cmp];
+    return ltr_cmp;
+}
+
+function key_val(){
+    if(val_key >= 65 && val_key <= 90)
+    {
+
+    }
+    else
+    {
+        
+    }
+}
