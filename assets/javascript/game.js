ed568982e5c6471aefbd772d3af06807c829498e
jmihalsky <jmihalsky@gmail.com>
2018-09-01 00:05:50 -0700

    major devlopment commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,9 @@
 var ltr_game = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var ltr_cmp = "";
+var psychic_wins = 0;
+var psychic_losses = 0;
+var guesses = 10;
+var psychic_guesses = "";
 
 function cmp_sel(){
     var cmp = Math.floor((Math.random()*25)+1);
@@ -10,10 +14,36 @@ function cmp_sel(){
 function key_val(){
     if(val_key >= 65 && val_key <= 90)
     {
+        if(start_key != ltr_cmp)
+        {
+            guesses--;
+            if (guesses == 0){
+                psy_loss();
+            }
+            else
+            {
+                document.getElementById("guesses_left").innerHTML = guesses;
+                psychic_guesses = start_key + " ";
+                document.getElementById("guesses").innerHTML = psychic_guesses;
+                document.getElementById("msg").innerHTML = "You're guess was not correct, try again."
+            }
+        }
+        else
+        {
 
+        }
     }
     else
     {
-        
+        document.getElementById("msg").innerHTML = "You need to select a letter!";
     }
+}
+
+function psy_loss(){
+    psychic_losses++;
+    guesses = 10;
+    document.getElementById("msg").innerHTML = "You lose. I have another number for you to guess, try again.";
+    document.getElementById("losses").innerHTML = psychic_losses;
+    cmp_sel();
+    document.getElementById("guesses_left").innerHTML = guesses;
 }
